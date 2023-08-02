c8333b469e55ec0f4336af0465ac0e3902df8574
gabrieleglvs <gabriele.desenvolvedora@gmail.com>
2023-08-02 16:37:44 -0300

    tratamento de erros com catch

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(rJSON => {
+    if (rJSON.erro) {
+        throw Error('Esse cep não existe!') //O erro do cep inválido vem no próprio Then, por isso tratamos aqui.
+    } else 
+    console.log(rJSON)
+})
+.catch(erro => console.log(erro)); //esse é um erro de rejected.
 
 console.log(consultaCEP)
